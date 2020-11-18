42dcda9b1b87038a6347ab9f064dddf5c08199e6
daoan <daoan1412@gmail.com>
2020-11-18 13:15:24 +0700

    13:15

--- ios/Classes/CameraKitFlutterView.swift
+++ ios/Classes/CameraKitFlutterView.swift
@@ -124,7 +124,6 @@ class CameraKitFlutterView : NSObject, FlutterPlatformView, AVCaptureVideoDataOu
                 self.headEulerAngle["maxY"] = (myArgs?["maxY"] as! Int)
                 self.headEulerAngle["minZ"] = (myArgs?["minZ"] as! Int)
                 self.headEulerAngle["maxZ"] = (myArgs?["maxZ"] as! Int)
-                print("set setFaceDetectionStrategy  minX \(self.headEulerAngle["minX"])")
              }           
             })
     }
@@ -477,9 +476,10 @@ class CameraKitFlutterView : NSObject, FlutterPlatformView, AVCaptureVideoDataOu
                     return
                 }
 
-                if let data = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer,
-                             previewPhotoSampleBuffer: nil) {
-                    channel.invokeMethod("onFaceImageCallBack", arguments: FlutterStandardTypedData(bytes: data))
+                if let image = imageFromImageBuffer(imageBuffer, faceFrame: frame) {
+                    if let data = image.jpegData(compressionQuality: 0.8) {
+                         channel.invokeMethod("onFaceImageCallBack", arguments: FlutterStandardTypedData(bytes: data))
+                    }
                 }
                 
             }
@@ -506,6 +506,48 @@ class CameraKitFlutterView : NSObject, FlutterPlatformView, AVCaptureVideoDataOu
             }
         }
         
+    }
+
+     func imageFromImageBuffer(_ imageBuffer : CVImageBuffer, faceFrame: CGRect) -> UIImage?
+    {
+        let rect = CGRect(
+            x: faceFrame.origin.x,
+            y: faceFrame.origin.y,
+            width: faceFrame.size.width,
+            height: faceFrame.size.height
+        )
+        // Lock the base address of the pixel buffer
+        CVPixelBufferLockBaseAddress(imageBuffer, CVPixelBufferLockFlags.readOnly)
+        
+        // Get the number of bytes per row for the pixel buffer
+        let baseAddress = CVPixelBufferGetBaseAddress(imageBuffer)
+        
+        // Get the number of bytes per row for the pixel buffer
+        let bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer)
+        // Get the pixel buffer width and height
+        let width = CVPixelBufferGetWidth(imageBuffer)
+        let height = CVPixelBufferGetHeight(imageBuffer)
+        
+        // Create a device-dependent RGB color space
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        // Create a bitmap graphics context with the sample buffer data
+        var bitmapInfo: UInt32 = CGBitmapInfo.byteOrder32Little.rawValue
+        bitmapInfo |= CGImageAlphaInfo.premultipliedFirst.rawValue & CGBitmapInfo.alphaInfoMask.rawValue
+        //let bitmapInfo: UInt32 = CGBitmapInfo.alphaInfoMask.rawValue
+        let context = CGContext.init(data: baseAddress, width: width, height: height, bitsPerComponent: 8, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo)
+        // Create a Quartz image from the pixel data in the bitmap graphics context
+        let quartzImage = context?.makeImage()
+        // Unlock the pixel buffer
+        CVPixelBufferUnlockBaseAddress(imageBuffer, CVPixelBufferLockFlags.readOnly)
+        
+        // Create an image object from the Quartz image
+        
+        let faceCgImage = quartzImage!.cropping(to: rect)!
+        
+        let image = UIImage.init(cgImage: faceCgImage, scale: 1, orientation: imageOrientation(fromDevicePosition: cameraPosition))
+        
+        return image
     }
     
     func barcodeRead(barcode: String) {
